b57a98e933cba4d37612b86e99e271e535615da6
Janae Quinones <janaemassa@gmail.com>
2021-03-20 21:09:08 -0400

    done.;

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,21 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    # using json: birds is actually json: birds.to_json. Works here bc of rails magic
+    render json: birds, only: [:id, :name, :species]
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    # if bird exists, render the json data
+    if bird
+      # render json: birds - renders specific bird with all keys
+      # render json: {id: bird.id, name: bird.name, species: bird.species} - renders specific bird with specific keys
+      # render json: bird.slice(:id, :name, :species) - same as above
+      render json: bird, only: [:id, :name, :species] # same as above, best practice. can also use except: in place of only: to exclude specific keys
+    # else display error message
+    else
+      render json: {message: 'Bird not found'}
+    end
   end
 end
